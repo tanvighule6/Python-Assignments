6c5ee4c86960da971b0f1c4cae02f72c1767ab0b
tanvighule6 <tanvighule2007@gmail.com>
2026-03-22 20:27:24 +0530

    Create program2_set.py

---
+++ Unit-1/program2_set.py
@@ -0,0 +1,9 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+print("Union:", set1.union(set2))
+print("Intersection:", set1.intersection(set2))
+print("Difference (set1 - set2):", set1.difference(set2))
