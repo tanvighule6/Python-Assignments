5b29d70f57956c86ba3e073f986ac9b626dbcf79
tanvighule6 <tanvighule2007@gmail.com>
2026-03-22 20:28:40 +0530

    Create program3_tuple.py

---
+++ Unit-1/program3_tuple.py
@@ -0,0 +1,14 @@
+tuple1 = (10, 20, 30, 40)
+
+print("Tuple elements:", tuple1)
+print("First element:", tuple1[0])
+
+nested_tuple = (tuple1, (50, 60))
+print("Nested Tuple:", nested_tuple)
+
+repeated_tuple = tuple1 * 2
+print("Repeated Tuple:", repeated_tuple)
+
+tuple2 = (70, 80)
+concatenated_tuple = tuple1 + tuple2
+print("Concatenated Tuple:", concatenated_tuple)
