3956487472433d3539dcf4dd5a65f235ea17e789
tanvighule6 <tanvighule2007@gmail.com>
2026-03-22 20:24:45 +0530

    Create program1_list.py

---
+++ Unit-1/program1_list.py
@@ -0,0 +1,16 @@
+my_list = [10, 20, 30, 40, 50]
+
+print("Original List:", my_list)
+print("First element:", my_list[0])
+
+my_list.append(60)
+print("After adding element:", my_list)
+
+my_list.remove(30)
+print("After removing element:", my_list)
+
+my_list.sort()
+print("Sorted list:", my_list)
+
+my_list.reverse()
+print("Reversed list:", my_list)
