d2d86bf00b680861b251dd40342824862b3dedba
Tanvi Ghule <tanvighule2007@gmail.com>
2026-04-12 23:53:45 +0530

    Create queue_safe_dequeue.py

---
+++ Unit-5/Program 3/queue_safe_dequeue.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, item):
+        self.queue.append(item)
+        print(item, "added to queue")
+
+    def safe_dequeue(self):
+        if len(self.queue) == 0:
+            print("Queue is empty, cannot dequeue.")
+            return None
+        else:
+            return self.queue.popleft()
+
+    def display(self):
+        print("Queue:", list(self.queue))
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.display()
+
+print("Dequeued element:", q.safe_dequeue())
+print("Dequeued element:", q.safe_dequeue())
+print("Dequeued element:", q.safe_dequeue())  
